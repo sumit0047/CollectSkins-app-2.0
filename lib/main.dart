ee5d3573d60dd3f39bbe7633b0d084f417e652c1
Sumit <sumit.kr445@gmail.com>
2020-06-09 19:16:32 +0530

    Added fragments as navigation pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:collectskins/screens/Earn.dart';
 import 'package:collectskins/widgets/ChatMessage.dart';
 import 'package:collectskins/widgets/InputMessage.dart';
 import 'package:flutter/material.dart';
@@ -18,13 +19,48 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class DrawerItem {
+  String title;
+  IconData icon;
+  DrawerItem(this.title, this.icon);
+}
+
 class MyHomePage extends StatefulWidget {
+
+  final drawerItems = [
+    new DrawerItem("Home", Icons.home),
+    new DrawerItem("Earn", Icons.monetization_on),
+    new DrawerItem("Shop", Icons.shop),
+    new DrawerItem("Top", Icons.vertical_align_top),
+    new DrawerItem("Blog", Icons.shop),
+    new DrawerItem("Shop", Icons.short_text),
+    new DrawerItem("Help", Icons.help)
+  ];
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  int _selectedDrawerIndex = 0;
+
+  _getDrawerItemWidget(int pos) {
+    switch (pos) {
+      case 0:
+        return new Home();
+      case 1:
+        return new Earn();
+
+      default:
+        return new Text("Error");
+    }
+  }
+
+  _onSelectItem(int index) {
+    setState(() => _selectedDrawerIndex = index);
+    Navigator.of(context).pop(); // close the drawer
+  }
 
 
   @override
@@ -36,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("CollectSkins"),
         backgroundColor: Color(0xff2f3136),
       ),
-      body: Home(),
+      body: _getDrawerItemWidget(_selectedDrawerIndex),
       drawer : SizedBox(
         width: MediaQuery.of(context).size.width * 0.55,
         child: Drawer(
@@ -44,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             children: <Widget>[
               Container(
-                height : 200,
+                height : MediaQuery.of(context).size.height * 0.25,
                 color: Color(0xff2f3136),
                 child: Column(
                   children: <Widget>[
@@ -94,51 +130,28 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
 
 
-              Expanded(
-                // ListView contains a group of widgets that scroll inside the drawer
-                  child: Container(
+                    Container(
+                        height : MediaQuery.of(context).size.height * 0.65,
                     color: const Color(0xff2f3136),
-                    child: Column(
-                      children: <Widget>[
-                        new ListTile(
-                          title: new Text("Home",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.home,color: Colors.white70,),
-                        ),
-                        new ListTile(
-                          title: new Text("Earn",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.monetization_on,color: Colors.white70,),
-                        ),
-                        new ListTile(
-                          title: new Text("Shop",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.shop,color: Colors.white70,),
-                        ),
-                        new ListTile(
-                          title: new Text("Top",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.vertical_align_top,color: Colors.white70,),
-                        ),
-                        new ListTile(
-                          title: new Text("Games",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.games,color: Colors.white70,),
-                        ),
-                        new ListTile(
-                          title: new Text("Blog",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.short_text,color: Colors.white70,),
-                        ),
-                        new ListTile(
-                          title: new Text("Help",style: TextStyle(color: Colors.white70),),
-                          leading: new Icon(Icons.help,color: Colors.white70,),
-                        ),
-                      ],
-                    ),
-                  )
+                    child:
+                        ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: widget.drawerItems.length,
+                            itemBuilder: (BuildContext contxt, int Index) {
+                              return new ListTile(
+                                title: new Text(widget.drawerItems[Index].title,style: TextStyle(color: Colors.white70),),
+                                leading: new Icon(widget.drawerItems[Index].icon,color: Colors.white70,),
+                                selected: Index == _selectedDrawerIndex,
+                                onTap: () => _onSelectItem(Index),
+                              );
+                            }
+                        )
+
+
               ),
               // This container holds the align
               Container(
-                // This align moves the children to the bottom
-                  child: Align(
-                      alignment: FractionalOffset.bottomCenter,
-                      // This container holds all the children that will be aligned
-                      // on the bottom and should not scroll with the above ListView
+                  height : MediaQuery.of(context).size.height * 0.10,
                       child: Container(
                           color: const Color(0xff2f3136),
                           child: Column(
@@ -150,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             ],
                           )
                       )
-                  )
+
               )
             ],
           ),
